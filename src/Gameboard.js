a294c41c06f437b6b9d8a6fd653860e827da2f22
muha <1student.muhammed1@gmail.com>
2023-08-07 22:25:13 +0300

    feat:refactor the code(factory function --> class)

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,83 +1,63 @@
 import { Ship } from './ship';
-const array10x10 = [];
-const missedAttack = [];
 
-export function createGameboard() {
+export class createGameboard {
+  constructor() {
+    this.array10x10 = [];
+    this.ships = [];
+    this.missedAttack = [];
 
-  const ships = [];
-
-  const row = 10;
-  const column = 10;
-
-  //creating array of 10 * 10 
-  for (let index = 0; index < row; index++) {
-    const newRow = [];
-    for (let j = 0; j < column; j++) {
-      newRow.push(null)
+    for (let i = 0; i < 10; i++) {
+      const newRow = new Array(10).fill(null);
+      this.array10x10.push(newRow);
     }
-    array10x10.push(newRow)
   }
 
-  function placeShipAt(x, y, length) {
-    const newShip = Ship(length);// creating new ship
+  placeShipAt(x, y, length) {
+    const newShip = new Ship(length);
 
-    if (x >= 10 || y >= 10) {
-      console.log('cant place ship outside of the board');
+    if (x < 0 || x >= 10 || y < 0 || y >= 10) {
+      console.log('Cannot place ship outside of the board');
       return;
     }
 
-    // Check if there is a ship already at this location
-    if (array10x10[x][y] !== null) {
-      console.log("Can't place a ship twice");
-      return "none";
+    if (this.array10x10[x][y] !== null) {
+      console.log("Cannot place a ship twice");
+      return;
     }
 
-    array10x10[x][y] = newShip;// set the ship in this[x][y] specific cords  
-    ships.push(newShip) // pushing the new ship at the ships array
-
+    this.array10x10[x][y] = newShip;
+    this.ships.push(newShip);
   }
 
-  
-  function receiveAttack(x, y) {
+  receiveAttack(x, y) {
     if (x >= 0 && x < 10 && y >= 0 && y < 10) {
-      const shipToAttack = array10x10[x][y];
-      //checking if there is a ship
-      if (shipToAttack == null) {
-        console.log('NO Ship there is');
-        missedAttack.push({ x, y });
-        return;
+      const shipToAttack = this.array10x10[x][y];
+      if (!shipToAttack) {
+        console.log('No ship there');
+        this.missedAttack.push({ x, y });
+        return false;
       }
 
-      console.log(`hits: ${shipToAttack.hit()}`)
-      console.log(`IS Sunk: ${shipToAttack.isSunk()}`)
-
+      console.log(`hits: ${shipToAttack.hit()}`);
+      console.log(`Is Sunk: ${shipToAttack.isSunk()}`);
+      console.log(`hit (${x}, ${y})`)
+      return true;
     } else {
-      console.log('invalid coordinates. Attack inside the gameBoard')
+      console.log('Invalid coordinates. Attack inside the gameBoard');
     }
-
   }
 
-  function checkingBoard() {
-    return ships.every(ship => ship.isSunk());
+  checkingBoard() {
+    return this.ships.every(ship => ship.isSunk());
   }
 
-  function displayMissedAttacks() {
-    if (missedAttack.length !== 0) {
-      console.log('Missed Attacks: ')
-      for (const missed of missedAttack) {
-        console.log(`(${missed.x}, ${missed.y})`);
+  displayMissedAttacks() {
+    if (this.missedAttack.length !== 0) {
+      console.log('Missed Attacks:');
+      for (const missed of this.missedAttack) {
+        return console.log(`(${missed.x}, ${missed.y})`);
       }
     }
-  };
-
-
-  return {
-    placeShipAt,
-    receiveAttack,
-    checkingBoard,
-    displayMissedAttacks,
-    array10x10,
-    missedAttack
+    return "No Missing Attack"
   }
-
 }
