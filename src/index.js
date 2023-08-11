753a6017910d92b9c361120875e5d2c388dc79b9
muha <1student.muhammed1@gmail.com>
2023-08-11 17:01:20 +0300

    chore:set initial player and  comp & render boards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import { Player } from './Player';
 import { renderGameBoardOF, stylingBoards, computerBoard, playerBoard, markCell, markComputerCell, updatePlayerBoardDOM, horizontalButton, verticalButton } from './Dom';
 import { createGameboard } from './Gameboard';
+
+// Step 1: Create players
+const player = new Player();
+const computer = new Player();
+
+// Step 2: Set up game boards
+export const playerGameboard = new createGameboard();
+const computerGameboard = new createGameboard();
+
+// Step 3: Render game boards
+renderGameBoardOF(playerBoard);
+renderGameBoardOF(computerBoard);
